8bb3c69b79c2dfca6d2e212f555c1d834f8418f4
Andrew Tergis <theterg@gmail.com>
2023-06-24 16:12:47 -0400

    Add new weather app, slow down to 15 minute poll

--- push_all.sh
+++ push_all.sh
@@ -5,3 +5,5 @@ device="dismally-deserving-fortified-chub-f32"
 cd community/
 pixlet render apps/nycairquality/nyc_air_quality.star 
 pixlet push --installation-id nycairquality ${device} apps/nycairquality/nyc_air_quality.webp
+pixlet render apps/weatherplots/weather_plots.star location="{\"lat\":40.68,\"lng\":-73.97,\"timezone\":\"America/New_York\"}" window=9 
+pixlet push --installation-id weatherplots ${device} apps/weatherplots/weather_plots.webp
